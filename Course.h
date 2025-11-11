f4e151fcebeb52624460988ca1788aa463fea0e4
aminasamer <aminasamer2006@gmail.com>
2025-11-11 09:34:27 +0200

    Implement Course class with student management

--- Course.h
+++ Course.h
@@ -3,20 +3,47 @@
 
 #include <string>
 #include "Student.h"
+#include <iostream>
 using namespace std;
-//#write your code here
+class Course{
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    Course(){
 
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+    Course(int CC, string CN, int max){
+        courseCode = CC;
+        courseName = CN;
+        maxStudents = max;
+        students[] = malloc(sizeof(Student)*maxStudents);
+        currentStudents = 0;
+    }
+    void addStudent(const Student& s){
+        students[currentStudents-1] = s;
+        currentStudents++;
+    }
+    string getCourseName(){
+        return courseName;
+    }
+    int getCourseCode(){
+        return courseCode;
+    }
+    int getMaxStudents(){
+        return maxStudents;
+    }
+    Student getStudents(){
+        return students;
+    }
+    void display(){
+        cout<<"Course: "<<getCourseCode()<<" - "<<getCourseName()<<endl;
+        cout<<"Max Students: "<<getMaxStudents()<<endl;
+        cout<<"Currently Enrolled: "<<getStudents()<<endl;
+    }
+}
 
 #endif
