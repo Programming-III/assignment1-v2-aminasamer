1a548af1c04b2697a022bf4c9bcb9c8c4fa9f1f3
aminasamer <aminasamer2006@gmail.com>
2025-11-11 09:42:08 +0200

    Refactor Course class by removing implementation details

--- Course.h
+++ Course.h
@@ -13,37 +13,14 @@ class Course{
     Student* students;
     int currentStudents;
     public:
-    Course(){
-
-    }
-    Course(int CC, string CN, int max){
-        courseCode = CC;
-        courseName = CN;
-        maxStudents = max;
-        students[] = malloc(sizeof(Student)*maxStudents);
-        currentStudents = 0;
-    }
-    void addStudent(const Student& s){
-        students[currentStudents-1] = s;
-        currentStudents++;
-    }
-    string getCourseName(){
-        return courseName;
-    }
-    int getCourseCode(){
-        return courseCode;
-    }
-    int getMaxStudents(){
-        return maxStudents;
-    }
-    Student getStudents(){
-        return students;
-    }
-    void display(){
-        cout<<"Course: "<<getCourseCode()<<" - "<<getCourseName()<<endl;
-        cout<<"Max Students: "<<getMaxStudents()<<endl;
-        cout<<"Currently Enrolled: "<<getStudents()<<endl;
-    }
+    Course();
+    Course(int CC, string CN, int max);
+    void addStudent(const Student& s);
+    string getCourseName();
+    int getCourseCode();
+    int getMaxStudents();
+    Student getStudents();
+    void display();
 }
 
 #endif
