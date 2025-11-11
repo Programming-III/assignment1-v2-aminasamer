9f0e5bf80107cf468557b76174292e77d8a8ffd3
aminasamer <aminasamer2006@gmail.com>
2025-11-11 08:59:08 +0200

    Define Person class with basic methods

--- Person.h
+++ Person.h
@@ -1,20 +1,22 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include <iostream>
 #include <string>
 using namespace std;
-
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
-
+class Person{
+private:
+string name;
+int id;
+public:
+string getName(){
+return name;
+}
+int getId(){
+return id;
+}
+void display(){
+  cout<<"Name: "<<getName()<<endl;
+  cout<<"ID: "<<getId()<<endl;
+}
+};
 #endif
