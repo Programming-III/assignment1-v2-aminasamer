2564af358573e80aa8283cb01d3dfae0d80b85a6
aminasamer <aminasamer2006@gmail.com>
2025-11-11 09:25:15 +0200

    Add Student class with year level and major
    Implemented Student class inheriting from Person with attributes for year level and major, including constructors and display method.

--- Student.h
+++ Student.h
@@ -2,20 +2,31 @@
 #define STUDENT_H
 
 #include "Person.h"
+#include <iostream>
 #include <string>
-#using namespace std;
-//#write student class here
+using namespace std;
+class Student :: public Person{
+  private:
+  int yearLevel;
+  string major;
+  public:
+  Student(){
 
-
-
-
-
-
-
-
-
-
-
-
-
+  }
+  Student(int year, string m){
+    yearLevel = year;
+    major = m;
+  }
+  int getYearLevel(){
+  return yearLevel;
+  }
+  string getMajor(){
+  return major;
+  }
+  void display(){
+    cout<<"Name: "<<getName()<<endl;
+    cout<<"Year: "<<getYearLevel()<<endl;
+    cout<<"Major: "<<getMajor()<<endl;
+  }
+}
 #endif
